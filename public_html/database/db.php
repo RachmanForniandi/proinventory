04c3b05ffffb2c02cf20c1c395edd504bd95e3f7
RachmanForniandi <rachmanforniandi@gmail.com>
2018-01-31 16:04:56 +0700

    first commit buat template dan database

---
+++ public_html/database/db.php
@@ -0,0 +1,23 @@
+<?php 
+
+/**
+* 
+*/
+class Database {
+	
+	private $con;
+
+	public function connect(){
+		include_once("constants.php");
+		$this->con = new Mysqli(HOST,USER,PASS,DB);
+		if ($this->con) {
+			echo "Database connected.";
+			return $this->con;
+		}
+		return "DATABASE_CONNECTION_FAIL !!";
+	}
+}
+/*$db = new Database();
+$db->connect();*/
+
+?>
