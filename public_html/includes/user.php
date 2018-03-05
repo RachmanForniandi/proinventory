f25d7c85b5216f4ca79b78e4955e66bf87ffd8b8
RachmanForniandi <rachmanforniandi@gmail.com>
2018-03-05 17:21:35 +0700

    test user register system

--- public_html/includes/user.php
+++ public_html/includes/user.php
@@ -18,7 +18,7 @@ class User {
 		$pre_stmt->bind_param("s",$email);
 		$pre_stmt->execute() OR DIE($this->con->error);
 		$result = $pre_stmt->get_result();
-		if ($result > 0) {
+		if ($result-> num_rows > 0) {
 			return 1;
 		}else{
 			return 0;
@@ -28,9 +28,27 @@ class User {
 	public function createUserAccount($username,$email,$password,$usertype){
 		//utk melindungi aplikasi sql injection
 		//menggunakan prepare statement
-		$pre_stmt = $this->con->prepare("");
+		if ($this->emailExists($email)) {
+			return "EMAIL_ALREADY_EXISTS";
+		}else{
+			$pass_hash = password_hash($password,PASSWORD_BCRYPT,["cost"=>8]);
+			$date = date("Y-m-d");
+			$notes = "";
+			$pre_stmt = $this->con->prepare("INSERT INTO `user`(`username`, `email`, `password`, `user_level`, `register_date`, `last_login`, `notes`) VALUES (?,?,?,?,?,?,?)");
+			$pre_stmt->bind_param("sssssss",$username,$email,$pass_hash,$usertype,$date,$date,$notes);
+			$result = $pre_stmt->execute() OR DIE($this->con->error);
+			if ($result) {
+				return $this->con->insert_id;
+			}else{
+				return "SOME_ERROR";
+			}
+		}
+		
 	}
 }
+
+$user = new User();
+echo $user->createUserAccount("Test","rachmanforniandi1st@gmail.com","1234567890","Admin");
 
 
 ?>
